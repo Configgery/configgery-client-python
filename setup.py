cc07a02ed2d1348f084b9eda8d51e5b1c564bc1c
Robert Weiss <weiss.robbie@gmail.com>
2022-01-16 21:18:54 +1100

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="configgery-client",
-    version="1.0.1",
+    version="1.0.2",
     author="Configgery Pty Ltd",
     author_email="support@configgery.com",
     description="Python client for devices interacting with configgery.com",
