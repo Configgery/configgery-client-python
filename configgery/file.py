874855ea02d24da8ecdf0be3b8b312b461bee5b3
Robert Weiss <weiss.robbie@gmail.com>
2024-02-23 00:03:49 +1100

    Apply formatting

--- configgery/file.py
+++ configgery/file.py
@@ -9,13 +9,13 @@ logger = logging.getLogger(__name__)
 def file_md5(path: Path) -> str:
     # noinspection PyBroadException
     try:
-        with path.open('rb') as fp:
+        with path.open("rb") as fp:
             return hexlify(md5(fp.read()).digest()).decode()
     except (FileNotFoundError, PermissionError):
-        return ''
+        return ""
     except BaseException:
-        logger.exception(f'Unexpected exception when reading md5')
-        return ''
+        logger.exception(f"Unexpected exception when reading md5")
+        return ""
 
 
 def remove_subdirs_if_empty(root: Path):
--- tests/test_client.py
+++ tests/test_client.py
@@ -22,35 +22,35 @@ def configuration_directory() -> Path:
 
 def default_device_group_metadata(last_checked: Optional[datetime] = None):
     return {
-        'device_group_id': '85ffb504-cc91-4710-a0e7-e05599b19d0b',
-        'device_group_version': 1,
-        'configurations_metadata': [
+        "device_group_id": "85ffb504-cc91-4710-a0e7-e05599b19d0b",
+        "device_group_version": 1,
+        "configurations_metadata": [
             {
-                'configuration_id': 'e312aa23-f8a8-4142-9a21-be640be7e547',
-                'path': 'foo.json',
-                'md5': '99914b932bd37a50b983c5e7c90ae93b',
-                'version': 1,
+                "configuration_id": "e312aa23-f8a8-4142-9a21-be640be7e547",
+                "path": "foo.json",
+                "md5": "99914b932bd37a50b983c5e7c90ae93b",
+                "version": 1,
             },
             {
-                'configuration_id': '85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a',
-                'path': 'bar.json',
-                'md5': '3d29a75fcf0ed7dfff86d3db8f92fc69',
-                'version': 2,
-                'alias': 'abc.json',
+                "configuration_id": "85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a",
+                "path": "bar.json",
+                "md5": "3d29a75fcf0ed7dfff86d3db8f92fc69",
+                "version": 2,
+                "alias": "abc.json",
             },
         ],
-        'version': 1,
-        'last_checked': (last_checked or (datetime.now(tz=timezone.utc) - timedelta(days=1))).isoformat(),
+        "version": 1,
+        "last_checked": (last_checked or (datetime.now(tz=timezone.utc) - timedelta(days=1))).isoformat(),
     }
 
 
 def write_metadata(configuration_directory, metadata):
-    with configuration_directory.joinpath('configurations.json') as fp:
+    with configuration_directory.joinpath("configurations.json") as fp:
         fp.write_text(json.dumps(metadata, indent=2))
 
 
 def all_files_and_dirs(d):
-    return {f for f in chain(d.glob('**/*'), d.glob('*'))}
+    return {f for f in chain(d.glob("**/*"), d.glob("*"))}
 
 
 def test_init_no_previous_configurations(configuration_directory):
@@ -65,39 +65,42 @@ def test_init_with_configurations(configuration_directory):
         c = Client("fake_api_key", configuration_directory)
     assert c._device_group_metadata is not None
     assert c._device_group_metadata == DeviceGroupMetadata(
-        device_group_id=UUID('85ffb504-cc91-4710-a0e7-e05599b19d0b'),
+        device_group_id=UUID("85ffb504-cc91-4710-a0e7-e05599b19d0b"),
         device_group_version=1,
         configurations_metadata={
             ConfigurationMetadata(
-                configuration_id=UUID('e312aa23-f8a8-4142-9a21-be640be7e547'),
-                path='foo.json',
-                md5='99914b932bd37a50b983c5e7c90ae93b',
+                configuration_id=UUID("e312aa23-f8a8-4142-9a21-be640be7e547"),
+                path="foo.json",
+                md5="99914b932bd37a50b983c5e7c90ae93b",
                 version=1,
                 alias=None,
             ),
             ConfigurationMetadata(
-                configuration_id=UUID('85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a'),
-                path='bar.json',
-                md5='3d29a75fcf0ed7dfff86d3db8f92fc69',
+                configuration_id=UUID("85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a"),
+                path="bar.json",
+                md5="3d29a75fcf0ed7dfff86d3db8f92fc69",
                 version=2,
-                alias='abc.json',
+                alias="abc.json",
             ),
         },
-        last_checked=now
+        last_checked=now,
     )
 
 
 @pytest.mark.parametrize(
-    ('version', 'loaded',),
+    (
+        "version",
+        "loaded",
+    ),
     [
         (1, True),
         (2, False),
     ],
-    ids=['validVersion', 'invalidVersion']
+    ids=["validVersion", "invalidVersion"],
 )
 def test_init_with_wrong_file_version(configuration_directory, version, loaded):
     m = default_device_group_metadata()
-    m['version'] = version
+    m["version"] = version
     write_metadata(configuration_directory, m)
 
     c = Client("fake_api_key", configuration_directory)
@@ -109,7 +112,7 @@ def test_init_with_wrong_file_version(configuration_directory, version, loaded):
 
 def test_init_with_corrupt_file(configuration_directory):
     m = default_device_group_metadata()
-    del m['device_group_id']
+    del m["device_group_id"]
     write_metadata(configuration_directory, m)
 
     c = Client("fake_api_key", configuration_directory)
@@ -120,47 +123,47 @@ def test_outdated_configurations(configuration_directory):
     m = default_device_group_metadata()
     write_metadata(configuration_directory, m)
 
-    configuration_directory.joinpath('configurations').mkdir()
-    with configuration_directory.joinpath('configurations', m['configurations_metadata'][0]['path']).open('wb') as fp:
-        fp.write(b'{}')
-    with configuration_directory.joinpath('configurations', m['configurations_metadata'][1]['path']).open('wb') as fp:
-        fp.write(b'invalid_data')
+    configuration_directory.joinpath("configurations").mkdir()
+    with configuration_directory.joinpath("configurations", m["configurations_metadata"][0]["path"]).open("wb") as fp:
+        fp.write(b"{}")
+    with configuration_directory.joinpath("configurations", m["configurations_metadata"][1]["path"]).open("wb") as fp:
+        fp.write(b"invalid_data")
 
     c = Client("fake_api_key", configuration_directory)
     assert c.is_download_needed()
     outdated_configurations = list(c.outdated_configurations())
     assert len(outdated_configurations) == 1
-    assert outdated_configurations[0].configuration_id == UUID(m['configurations_metadata'][1]['configuration_id'])
+    assert outdated_configurations[0].configuration_id == UUID(m["configurations_metadata"][1]["configuration_id"])
 
 
 def test_remove_old_files_and_dirs(configuration_directory):
     m = default_device_group_metadata()
     write_metadata(configuration_directory, m)
 
-    configurations_dir = configuration_directory.joinpath('configurations')
+    configurations_dir = configuration_directory.joinpath("configurations")
     configurations_dir.mkdir()
 
-    with configurations_dir.joinpath('a.json').open('wb') as fp:
-        fp.write(b'hello world')
+    with configurations_dir.joinpath("a.json").open("wb") as fp:
+        fp.write(b"hello world")
 
-    configurations_dir.joinpath('dir1').mkdir()
-    with configurations_dir.joinpath('dir1/a.json').open('wb') as fp:
-        fp.write(b'hello world')
+    configurations_dir.joinpath("dir1").mkdir()
+    with configurations_dir.joinpath("dir1/a.json").open("wb") as fp:
+        fp.write(b"hello world")
 
-    with configurations_dir.joinpath('foo.json').open('wb') as fp:
-        fp.write(b'{}')
+    with configurations_dir.joinpath("foo.json").open("wb") as fp:
+        fp.write(b"{}")
 
-    with configurations_dir.joinpath('bar.json').open('wb') as fp:
-        fp.write(b'{\n}')
+    with configurations_dir.joinpath("bar.json").open("wb") as fp:
+        fp.write(b"{\n}")
 
-    configurations_dir.joinpath('dir1/dir2/dir3').mkdir(parents=True)
+    configurations_dir.joinpath("dir1/dir2/dir3").mkdir(parents=True)
 
     c = Client("fake_api_key", configuration_directory)
     c._remove_old_configurations()
 
     assert all_files_and_dirs(configurations_dir) == {
-        configurations_dir.joinpath('foo.json'),
-        configurations_dir.joinpath('bar.json'),
+        configurations_dir.joinpath("foo.json"),
+        configurations_dir.joinpath("bar.json"),
     }
 
 
@@ -175,25 +178,28 @@ def test_check_latest(configuration_directory):
         mock_poolmanager.request.side_effect = [
             FakeHTTPResponse(
                 status=200,
-                data=json.dumps({
-                    'device_group_id': '85ffb504-cc91-4710-a0e7-e05599b19d0b',
-                    'device_group_version': 1,
-                    'configurations': [
-                        {
-                            'configuration_id': 'e312aa23-f8a8-4142-9a21-be640be7e547',
-                            'path': 'foo.json',
-                            'md5': '99914b932bd37a50b983c5e7c90ae93b',
-                            'version': 1,
-                        },
-                        {
-                            'configuration_id': '85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a',
-                            'path': 'bar.json',
-                            'md5': '3d29a75fcf0ed7dfff86d3db8f92fc69',
-                            'version': 2,
-                            'alias': 'abc.json',
-                        },
-                    ],
-                }, indent=2).encode()
+                data=json.dumps(
+                    {
+                        "device_group_id": "85ffb504-cc91-4710-a0e7-e05599b19d0b",
+                        "device_group_version": 1,
+                        "configurations": [
+                            {
+                                "configuration_id": "e312aa23-f8a8-4142-9a21-be640be7e547",
+                                "path": "foo.json",
+                                "md5": "99914b932bd37a50b983c5e7c90ae93b",
+                                "version": 1,
+                            },
+                            {
+                                "configuration_id": "85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a",
+                                "path": "bar.json",
+                                "md5": "3d29a75fcf0ed7dfff86d3db8f92fc69",
+                                "version": 2,
+                                "alias": "abc.json",
+                            },
+                        ],
+                    },
+                    indent=2,
+                ).encode(),
             )
         ]
 
@@ -201,25 +207,25 @@ def test_check_latest(configuration_directory):
             assert c.check_latest()
 
     assert c._device_group_metadata == DeviceGroupMetadata(
-        device_group_id=UUID('85ffb504-cc91-4710-a0e7-e05599b19d0b'),
+        device_group_id=UUID("85ffb504-cc91-4710-a0e7-e05599b19d0b"),
         device_group_version=1,
         configurations_metadata={
             ConfigurationMetadata(
-                configuration_id=UUID('e312aa23-f8a8-4142-9a21-be640be7e547'),
-                path='foo.json',
-                md5='99914b932bd37a50b983c5e7c90ae93b',
+                configuration_id=UUID("e312aa23-f8a8-4142-9a21-be640be7e547"),
+                path="foo.json",
+                md5="99914b932bd37a50b983c5e7c90ae93b",
                 version=1,
                 alias=None,
             ),
             ConfigurationMetadata(
-                configuration_id=UUID('85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a'),
-                path='bar.json',
-                md5='3d29a75fcf0ed7dfff86d3db8f92fc69',
+                configuration_id=UUID("85d0acae-4a9c-49ce-b8dc-f8a41c6c6c6a"),
+                path="bar.json",
+                md5="3d29a75fcf0ed7dfff86d3db8f92fc69",
                 version=2,
-                alias='abc.json',
+                alias="abc.json",
             ),
         },
-        last_checked=now
+        last_checked=now,
     )
 
     with freeze_time(now + timedelta(hours=1)):
@@ -230,31 +236,25 @@ def test_download_new_configurations(configuration_directory):
     m = default_device_group_metadata()
     write_metadata(configuration_directory, m)
 
-    configurations_dir = configuration_directory.joinpath('configurations')
+    configurations_dir = configuration_directory.joinpath("configurations")
     configurations_dir.mkdir()
     # Ensure old configurations are deleted
-    with configurations_dir.joinpath('oldfile.json').open('wb') as fp:
-        fp.write(b'hello world')
+    with configurations_dir.joinpath("oldfile.json").open("wb") as fp:
+        fp.write(b"hello world")
 
     c = Client("fake_api_key", configuration_directory)
     assert c.is_download_needed()
     with MagicMock() as mock_poolmanager:
         c._pool = mock_poolmanager
         mock_poolmanager.request.side_effect = [
-            FakeHTTPResponse(
-                status=200,
-                data=b'{\n}'
-            ),
-            FakeHTTPResponse(
-                status=200,
-                data=b'{}'
-            ),
+            FakeHTTPResponse(status=200, data=b"{\n}"),
+            FakeHTTPResponse(status=200, data=b"{}"),
         ]
         assert c.download_configurations()
 
     assert all_files_and_dirs(configurations_dir) == {
-        configurations_dir.joinpath('foo.json'),
-        configurations_dir.joinpath('bar.json'),
+        configurations_dir.joinpath("foo.json"),
+        configurations_dir.joinpath("bar.json"),
     }
     download_needed = c.is_download_needed()
     if download_needed:
@@ -264,7 +264,7 @@ def test_download_new_configurations(configuration_directory):
 
 def test_make_parent_directories_for_configuration_metadata():
     with tempfile.TemporaryDirectory() as d:
-        configuration_directory = Path(d).joinpath('a/b/c')
+        configuration_directory = Path(d).joinpath("a/b/c")
         assert not configuration_directory.exists()
         _ = Client("fake_api_key", configuration_directory)
         assert configuration_directory.exists()
@@ -272,12 +272,14 @@ def test_make_parent_directories_for_configuration_metadata():
 
 def test_make_parent_directories_for_configuration_files(configuration_directory):
     m = default_device_group_metadata()
-    m['configurations_metadata'].append({
-        'configuration_id': '2bfb6125-96fd-402f-a585-1799612bf9cc',
-        'path': 'a/b/c/d.json',
-        'md5': '99914b932bd37a50b983c5e7c90ae93b',
-        'version': 1,
-    })
+    m["configurations_metadata"].append(
+        {
+            "configuration_id": "2bfb6125-96fd-402f-a585-1799612bf9cc",
+            "path": "a/b/c/d.json",
+            "md5": "99914b932bd37a50b983c5e7c90ae93b",
+            "version": 1,
+        }
+    )
     write_metadata(configuration_directory, m)
 
     c = Client("fake_api_key", configuration_directory)
@@ -285,29 +287,20 @@ def test_make_parent_directories_for_configuration_files(configuration_directory
     with MagicMock() as mock_poolmanager:
         c._pool = mock_poolmanager
         mock_poolmanager.request.side_effect = [
-            FakeHTTPResponse(
-                status=200,
-                data=b'{}'
-            ),
-            FakeHTTPResponse(
-                status=200,
-                data=b'{\n}'
-            ),
-            FakeHTTPResponse(
-                status=200,
-                data=b'{}'
-            ),
+            FakeHTTPResponse(status=200, data=b"{}"),
+            FakeHTTPResponse(status=200, data=b"{\n}"),
+            FakeHTTPResponse(status=200, data=b"{}"),
         ]
         assert c.download_configurations()
 
-    configurations_dir = configuration_directory.joinpath('configurations')
+    configurations_dir = configuration_directory.joinpath("configurations")
     assert all_files_and_dirs(configurations_dir) == {
-        configurations_dir.joinpath('a'),
-        configurations_dir.joinpath('a/b'),
-        configurations_dir.joinpath('a/b/c'),
-        configurations_dir.joinpath('a/b/c/d.json'),
-        configurations_dir.joinpath('foo.json'),
-        configurations_dir.joinpath('bar.json'),
+        configurations_dir.joinpath("a"),
+        configurations_dir.joinpath("a/b"),
+        configurations_dir.joinpath("a/b/c"),
+        configurations_dir.joinpath("a/b/c/d.json"),
+        configurations_dir.joinpath("foo.json"),
+        configurations_dir.joinpath("bar.json"),
     }
 
 
@@ -318,12 +311,7 @@ def test_update_state(configuration_directory):
     c = Client("fake_api_key", configuration_directory)
     with MagicMock() as mock_poolmanager:
         c._pool = mock_poolmanager
-        mock_poolmanager.request.side_effect = [
-            FakeHTTPResponse(
-                status=200,
-                data=b'OK'
-            )
-        ]
+        mock_poolmanager.request.side_effect = [FakeHTTPResponse(status=200, data=b"OK")]
         assert c.update_state(DeviceState.Configurations_Applied)
 
 
